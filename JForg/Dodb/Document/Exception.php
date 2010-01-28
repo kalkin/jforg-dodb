7cbda901594b10934cef1bb4d385c03c9f3d877a
Bahtiar `kalkin-` Gadimov <bahtiar@gadimov.de>
2010-01-28 16:07:20 +0100

    Added an JForg_Dodb_Document_Exception

---
+++ JForg/Dodb/Document/Exception.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Base exception class for JForg_Dodb_Document.
+ * 
+ * @package   JForg_Dodb
+ * @author    Bahtiar Gadimov <bahtiar@gadimov.de>
+ * @copyright (c) 2010 Bahtiar Gadimov
+ * @since     2010-01-28
+ */
+class JForg_Dodb_Document_Exception extends Solar_Exception
+{
+}
