cdaddb684fc41277013b6fa4d3a8bdf553abc63b
Bahtiar `kalkin-` Gadimov <bahtiar@gadimov.de>
2010-01-28 15:30:53 +0100

    Generated a test class for JForg_Dodb_Document

---
+++ Test/JForg/Dodb/Document.php
@@ -0,0 +1,210 @@
+<?php
+/**
+ * 
+ * Concrete class test.
+ * 
+ */
+class Test_JForg_Dodb_Document extends Solar_Test {
+    
+    /**
+     * 
+     * Default configuration values.
+     * 
+     * @var array
+     * 
+     */
+    protected $_Test_JForg_Dodb_Document = array(
+    );
+    
+    /**
+     * 
+     * Test -- Constructor.
+     * 
+     */
+    public function test__construct()
+    {
+        $actual = Solar::factory('JForg_Dodb_Document');
+        $expect = 'JForg_Dodb_Document';
+        $this->assertInstance($actual, $expect);
+    }
+    
+    /**
+     * 
+     * Test -- Magic call implements "get...()" and "set...()" for params listed in the method name.
+     * 
+     */
+    public function test__call()
+    {
+        $this->todo('stub');
+    }
+    
+    /**
+     * 
+     * Test -- Get a parameter value
+     * 
+     */
+    public function test__get()
+    {
+        $this->todo('stub');
+    }
+    
+    /**
+     * 
+     * Test -- Checks if a parameter is set
+     * 
+     */
+    public function test__isset()
+    {
+        $this->todo('stub');
+    }
+    
+    /**
+     * 
+     * Test -- Set a parameter value
+     * 
+     */
+    public function test__set()
+    {
+        $this->todo('stub');
+    }
+    
+    /**
+     * 
+     * Test -- Returns a string json representation of the object.
+     * 
+     */
+    public function test__toString()
+    {
+        $this->todo('stub');
+    }
+    
+    /**
+     * 
+     * Test -- Checks if a parameter is unset
+     * 
+     */
+    public function test__unset()
+    {
+        $this->todo('stub');
+    }
+    
+    /**
+     * 
+     * Test -- Return the current element
+     * 
+     */
+    public function testCurrent()
+    {
+        $this->todo('stub');
+    }
+    
+    /**
+     * 
+     * Test -- Returns the parameter names
+     * 
+     */
+    public function testFetchParameterNames()
+    {
+        $this->todo('stub');
+    }
+    
+    /**
+     * 
+     * Test -- Returns an array representation of this document
+     * 
+     */
+    public function testFetchRawData()
+    {
+        $this->todo('stub');
+    }
+    
+    /**
+     * 
+     * Test -- Returns the type of the document
+     * 
+     */
+    public function testGetType()
+    {
+        $this->todo('stub');
+    }
+    
+    /**
+     * 
+     * Test -- Are there some unsaved changes?
+     * 
+     */
+    public function testIsDirty()
+    {
+        $this->todo('stub');
+    }
+    
+    /**
+     * 
+     * Test -- Return the key of the current element
+     * 
+     */
+    public function testKey()
+    {
+        $this->todo('stub');
+    }
+    
+    /**
+     * 
+     * Test -- Move forward to next element
+     * 
+     */
+    public function testNext()
+    {
+        $this->todo('stub');
+    }
+    
+    /**
+     * 
+     * Test -- Populates the document with data
+     * 
+     */
+    public function testPopulate()
+    {
+        $this->todo('stub');
+    }
+    
+    /**
+     * 
+     * Test -- Reloads the Document from the database and updates it's values
+     * 
+     */
+    public function testReload()
+    {
+        $this->todo('stub');
+    }
+    
+    /**
+     * 
+     * Test -- Rewind the Iterator to the first element
+     * 
+     */
+    public function testRewind()
+    {
+        $this->todo('stub');
+    }
+    
+    /**
+     * 
+     * Test -- Saves the document to the dodb
+     * 
+     */
+    public function testSave()
+    {
+        $this->todo('stub');
+    }
+    
+    /**
+     * 
+     * Test -- Checks if current position is valid
+     * 
+     */
+    public function testValid()
+    {
+        $this->todo('stub');
+    }
+}
