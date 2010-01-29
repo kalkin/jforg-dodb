607fe5abc8f921dee2b2c17100dc1a72fd0fcb03
Bahtiar `kalkin-` Gadimov <bahtiar@gadimov.de>
2010-01-29 15:57:21 +0100

    Added adapter stub

---
+++ JForg/Dodb/Adapter/Couchdb.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * An adapter for Document interaction with the Couchdb database
+ * 
+ * @package   JForg_Couchdb
+ * @author    Bahtiar Gadimov <bahtiar@gadimov.de>
+ * @copyright (c) 2010 Bahtiar Gadimov
+ * @since     2010-01-28
+ */
+class JForg_Dodb_Adapter_Couchdb extends JForg_Dodb_Adapter
+{
+
+}
