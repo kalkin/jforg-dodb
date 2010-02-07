c7d1579fd9e4dae2a22d0532ca1f87aaf4404a82
Bahtiar `kalkin-` Gadimov <bahtiar@gadimov.de>
2010-02-07 22:08:44 +0100

    Added the abstract function getUuid()

--- JForg/Dodb/Adapter.php
+++ JForg/Dodb/Adapter.php
@@ -46,4 +46,6 @@ abstract class JForg_Dodb_Adapter extends Solar_Base
     public abstract function deleteCollection(JForg_Dodb_Document_Collection $collection);
 
     public abstract function documentToString(JForg_Dodb_Document $doc);
+
+    public abstract function getUuid($count = 1);
 }
