b1133e4b50d15938e9393e90db0ac3c92322132d
Bahtiar `kalkin-` Gadimov <bahtiar@gadimov.de>
2010-02-07 22:35:23 +0100

    Added lots of documentation

--- JForg/Dodb/Adapter.php
+++ JForg/Dodb/Adapter.php
@@ -29,23 +29,106 @@ abstract class JForg_Dodb_Adapter extends Solar_Base
             'type_safe'     => false,
             );
 
+    /**
+     * Fetchs a document by id and returns it as an instance of
+     * JForg_Dodb_Document
+     * 
+     * @param scalar $id The document id
+     * 
+     * @return JForg_Dodb_Document
+     * @author Bahtiar Gadimov <bahtiar@gadimov.de>
+     */
     public abstract function fetch($id);
 
+    /**
+     * Fetchs documents by ids and returns an instance of
+     * JForg_Dodb_Document_Collection containing instances of JForg_Dodb_Document s
+     * 
+     * @param array $ids Indexed array containing the documents ids
+     * 
+     * @return JForg_Dodb_Document_Collection containing all the documents as instances of JForg_Dodb_Document
+     * @author Bahtiar Gadimov <bahtiar@gadimov.de>
+     */
     public abstract function fetchCollection(array $ids);
 
+    /**
+     * Saves a document
+     * 
+     * @param JForg_Dodb_Document $doc The document
+     * 
+     * @return JForg_Dodb_Document
+     * @author Bahtiar Gadimov <bahtiar@gadimov.de>
+     */
     public abstract function save(JForg_Dodb_Document $doc);
 
+    /**
+     * Saves all documents in a JForg_Dodb_Document_Collection.
+     * 
+     * @param JForg_Dodb_Document_Collection $collection A collection of documents
+     * 
+     * @return JForg_Dodb_Document_Collection with all saved documents
+     * @author Bahtiar Gadimov <bahtiar@gadimov.de>
+     */
     public abstract function saveCollection(JForg_Dodb_Document_Collection $collection);
 
+    /**
+     * Reloads a document with up to date data
+     * 
+     * @param JForg_Dodb_Document $doc The document to reload
+     * 
+     * @return JForg_Dodb_Document
+     * @author Bahtiar Gadimov <bahtiar@gadimov.de>
+     */
     public abstract function reload(JForg_Dodb_Document $doc);
 
+    /**
+     * Reloads all the documents in a JForg_Dodb_Document_Collection with up to date data.
+     * 
+     * @param JForg_Dodb_Document_Collection $collection  The collection to reload
+     * 
+     * @return JForg_Dodb_Document_Collection
+     * @author Bahtiar Gadimov <bahtiar@gadimov.de>
+     */
     public abstract function reloadCollection(JForg_Dodb_Document_Collection $collection);
 
+    /**
+     * Deletes a document from the database
+     * 
+     * @param JForg_Dodb_Document $doc The document to delete
+     * 
+     * @return void
+     * @author Bahtiar Gadimov <bahtiar@gadimov.de>
+     */
     public abstract function delete(JForg_Dodb_Document $doc);
 
+    /**
+     * Deletes all documents in a JForg_Dodb_Document_Collection
+     * 
+     * @param JForg_Dodb_Document_Collection $collection The collection of documents to delete
+     * 
+     * @return void
+     * @author Bahtiar Gadimov <bahtiar@gadimov.de>
+     */
     public abstract function deleteCollection(JForg_Dodb_Document_Collection $collection);
 
+    /**
+     * Returns a database specific string representation of a document
+     * 
+     * @param JForg_Dodb_Document $doc The document
+     * 
+     * @return string
+     * @author Bahtiar Gadimov <bahtiar@gadimov.de>
+     */
     public abstract function documentToString(JForg_Dodb_Document $doc);
 
+    /**
+     * Returns one or many uuid. Those can be fetch from the database or just
+     * generated in some random way
+     * 
+     * @param mixed $count Optional, defaults to 1. 
+     * 
+     * @return array|scalar
+     * @author Bahtiar Gadimov <bahtiar@gadimov.de>
+     */
     public abstract function getUuid($count = 1);
 }
