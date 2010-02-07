1abee5a84d000aa4097fde5ab8c5b8c1092593e6
Bahtiar `kalkin-` Gadimov <bahtiar@gadimov.de>
2010-02-07 20:40:03 +0100

    Added an Dodb Adapters Factory

---
+++ JForg/Dodb.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * TODO: short description.
+ * 
+ * TODO: long description.
+ * 
+ * @package   JForg_Dodb
+ * @author    Bahtiar Gadimov <bahtiar@gadimov.de>
+ * @copyright (c) 2010 Bahtiar Gadimov
+ * @since     2010-01-28
+ */
+class JForg_Dodb extends Solar_Factory
+{
+
+    /**
+     * Default configuration values.
+     *
+     * @config string adapter The adapter class to use
+     * 
+     * @var mixed  Defaults to array(            ). 
+     * @since 2010-01-28
+     */
+    protected $_JForg_Dodb = array(
+        'adapter' = null;
+            );
+
+}
