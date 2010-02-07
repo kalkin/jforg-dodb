834327a343f08bac0233b8d6c6b13ae9318c049f
Bahtiar `kalkin-` Gadimov <bahtiar@gadimov.de>
2010-02-07 22:35:13 +0100

    Aded a stub class for a document collection

---
+++ JForg/Dodb/Document/Collection.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * TODO: short description.
+ * 
+ * TODO: long description.
+ * 
+ * @author    Bahtiar Gadimov <bahtiar@gadimov.de>
+ * @copyright (c) 2010 Bahtiar Gadimov
+ * @since     2010-02-07
+ */
+class JForg_Dodb_Document_Collection extends Solar_Base{
+
+}
