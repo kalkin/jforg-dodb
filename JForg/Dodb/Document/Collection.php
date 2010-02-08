ed9b96f0b48c2daeee36fe1edea10f2bc4a7f95d
Bahtiar `kalkin-` Gadimov <bahtiar@gadimov.de>
2010-02-08 21:11:01 +0100

    Added @package tag to the Collection class

--- JForg/Dodb/Document/Collection.php
+++ JForg/Dodb/Document/Collection.php
@@ -6,6 +6,7 @@
  * 
  * @author    Bahtiar Gadimov <bahtiar@gadimov.de>
  * @copyright (c) 2010 Bahtiar Gadimov
+ * @package   JForg_Dodb
  * @since     2010-02-07
  */
 class JForg_Dodb_Document_Collection extends Solar_Base{
