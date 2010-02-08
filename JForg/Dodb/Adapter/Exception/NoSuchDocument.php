da0f290b0ea7e33655b752f972f29b8d599bbea5
Bahtiar `kalkin-` Gadimov <bahtiar@gadimov.de>
2010-02-08 21:10:42 +0100

    Added @package tag to NoSuchDocument exception

--- JForg/Dodb/Adapter/Exception/NoSuchDocument.php
+++ JForg/Dodb/Adapter/Exception/NoSuchDocument.php
@@ -3,6 +3,7 @@
  * Is thrown when a document isn't found.
  * 
  * @author    Bahtiar Gadimov <bahtiar@gadimov.de>
+ * @package   JForg_Dodb
  * @copyright (c) 2010 Bahtiar Gadimov
  * @since     2010-02-08
  */
