4d2924858192907934148f774661cc43461a90ee
Bahtiar `kalkin-` Gadimov <bahtiar@gadimov.de>
2010-02-08 22:31:14 +0100

    Implemented an JForg_Dodb_Array class
    - It implements an iterator and it has a populate() function

---
+++ JForg/Dodb/Array.php
@@ -0,0 +1,109 @@
+<?php
+/**
+ * Contains a document and keys which were used to find it.
+ * 
+ * @author    Bahtiar Gadimov <bahtiar@gadimov.de>
+ * @copyright (c) 2010 Bahtiar Gadimov
+ * @since     2010-02-08
+ */
+class JForg_Dodb_Array extends Solar_Base implements Iterator
+{
+
+	/**
+	 * The document data
+	 * 
+	 * @var array  Defaults to array(). 
+	 * @since 2010-01-26
+	 */
+	protected $_data = array();
+
+    /**
+     * The iterator position
+     *
+     * @see JForg_Dodb_Array::rewinddir()
+     * @see JForg_Dodb_Array::current() 
+     * @var int  Defaults to 0. 
+     * @since 2010-02-07
+     */
+    protected $_iterator_position = 0;
+
+    /**
+     * An array containing all property names, used for the Iterator interface.
+     * 
+     * @var int  Defaults to 0. 
+     * @since 2010-02-07
+     */
+    protected $_iterator_keys   = null;
+
+    /**
+     * Populates JForg_Dodb_Array with data
+     * 
+     * @param array $data 
+     * 
+     * @return void
+     * @author Bahtiar Gadimov <bahtiar@gadimov.de>
+     */
+    public function populate(array $data)
+    {
+        $this->_data = $data;
+    }
+
+    /**
+     * Return the current element
+     * 
+     * @return mixed
+     * @author Bahtiar Gadimov <bahtiar@gadimov.de>
+     */
+    public function current()
+    {
+        return $this->_data[$this->_iterator_keys[$this->_iterator_position]];
+    }
+
+    /**
+     * Return the key of the current element
+     * 
+     * @return scalar
+     * @author Bahtiar Gadimov <bahtiar@gadimov.de>
+     */
+    public function key()
+    {
+        return $this->_iterator_keys[$this->_iterator_position];
+    }
+
+    /**
+     * Move forward to next element
+     * 
+     * @return void
+     * @author Bahtiar Gadimov <bahtiar@gadimov.de>
+     */
+    public function next()
+    {
+        $this->_iterator_position++;
+    }
+
+    /**
+     * Rewind the Iterator to the first element
+     * 
+     * @return void
+     * @author Bahtiar Gadimov <bahtiar@gadimov.de>
+     */
+    public function rewind()
+    {
+        $this->_iterator_position = 0;
+        $this->_iterator_keys = $this->fetchPropetiesNames();
+    }
+
+    /**
+     * Checks if current position is valid
+     * 
+     * @return boolean
+     * @author Bahtiar Gadimov <bahtiar@gadimov.de>
+     */
+    public function valid()
+    {
+        if ( count($this->_iterator_position) > $this->_iterator_position )
+            return true;
+        return false;
+    }
+
+}
--- JForg/Dodb/Document.php
+++ JForg/Dodb/Document.php
@@ -7,7 +7,7 @@
  * @copyright (c) 2010 Bahtiar Gadimov
  * @since     2010-01-26
  */
-class JForg_Dodb_Document extends Solar_Base implements Iterator
+class JForg_Dodb_Document extends JForg_Dodb_Array implements Iterator
 {
 
 	/**
@@ -77,14 +77,6 @@ class JForg_Dodb_Document extends Solar_Base implements Iterator
      */
     protected $_sheme = null;
 
-	/**
-	 * The document data
-	 * 
-	 * @var array  Defaults to array(). 
-	 * @since 2010-01-26
-	 */
-	protected $_data = array();
-
     /**
      * Contains special properties of a document, i.g. for Couchdb this would
      * be stuff like _rev, _attachment ect..
@@ -101,24 +93,6 @@ class JForg_Dodb_Document extends Solar_Base implements Iterator
 	 * @since 2010-01-26
 	 */
 	protected $_dodb = null;
-
-    /**
-     * The iterator position
-     *
-     * @see JForg_Dodb_Document::rewinddir()
-     * @see JForg_Dodb_Document::current() 
-     * @var int  Defaults to 0. 
-     * @since 2010-02-07
-     */
-    protected $_iterator_position = 0;
-
-    /**
-     * An array containing all property names, used for the Iterator interface.
-     * 
-     * @var int  Defaults to 0. 
-     * @since 2010-02-07
-     */
-    protected $_iterator_keys   = null;
 
     /**
      * Post-construction tasks to complete object construction.
@@ -496,61 +470,4 @@ class JForg_Dodb_Document extends Solar_Base implements Iterator
         return $this->addSpecialProperty($name, $value);
     }
 
-    /**
-     * Return the current element
-     * 
-     * @return mixed
-     * @author Bahtiar Gadimov <bahtiar@gadimov.de>
-     */
-    public function current()
-    {
-        return $this->_data[$this->_iterator_keys[$this->_iterator_position]];
-    }
-
-    /**
-     * Return the key of the current element
-     * 
-     * @return scalar
-     * @author Bahtiar Gadimov <bahtiar@gadimov.de>
-     */
-    public function key()
-    {
-        return $this->_iterator_keys[$this->_iterator_position];
-    }
-
-    /**
-     * Move forward to next element
-     * 
-     * @return void
-     * @author Bahtiar Gadimov <bahtiar@gadimov.de>
-     */
-    public function next()
-    {
-        $this->_iterator_position++;
-    }
-
-    /**
-     * Rewind the Iterator to the first element
-     * 
-     * @return void
-     * @author Bahtiar Gadimov <bahtiar@gadimov.de>
-     */
-    public function rewind()
-    {
-        $this->_iterator_position = 0;
-        $this->_iterator_keys = $this->fetchPropetiesNames();
-    }
-
-    /**
-     * Checks if current position is valid
-     * 
-     * @return boolean
-     * @author Bahtiar Gadimov <bahtiar@gadimov.de>
-     */
-    public function valid()
-    {
-        if ( count($this->_iterator_position) > $this->_iterator_position )
-            return true;
-        return false;
-    }
 }
