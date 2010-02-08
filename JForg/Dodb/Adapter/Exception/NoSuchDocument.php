ce3ed790b03aa1c3e8eccd9b539e9123dd84d102
Bahtiar `kalkin-` Gadimov <bahtiar@gadimov.de>
2010-02-08 21:03:35 +0100

    Added an NoSuchDocument Exception

---
+++ JForg/Dodb/Adapter/Exception/NoSuchDocument.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Is thrown when a document isn't found.
+ * 
+ * @author    Bahtiar Gadimov <bahtiar@gadimov.de>
+ * @copyright (c) 2010 Bahtiar Gadimov
+ * @since     2010-02-08
+ */
+class JForg_Dodb_Adapter_Exception_NoSuchDocument extends Solar_Exception
+{
+
+}
