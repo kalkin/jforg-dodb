f8afcdbf501bf2c16c73c0e733490e84face0568
Bahtiar `kalkin-` Gadimov <bahtiar@gadimov.de>
2010-02-09 23:10:13 +0100

    Added function getKey() to JForg_Dodb_Record

--- JForg/Dodb/Record.php
+++ JForg/Dodb/Record.php
@@ -19,20 +19,32 @@ class JForg_Dodb_Record extends Solar_Base {
      */
     protected $_document = null;
 
+
+    /**
+     * The key
+     * 
+     * @var mixed  Defaults to null. 
+     * @since 2010-02-09
+     */
+    public $_key = null;
+
     /**
      * Populates the record with a document. This function can only be called
      * one time.
      * 
+     * @param mixed $key The key for corresponding JForg_Dodb_Document
      * @param JForg_Dodb_Document $doc Add the doc to a record
      * 
      * @return JForg_Dodb_Record
      * @author Bahtiar Gadimov <bahtiar@gadimov.de>
      */
-    public function populate(JForg_Dodb_Document $doc )
+    public function populate($key, JForg_Dodb_Document $doc )
     {
         if ( $this->_document == null )
+        {
             $this->_document = $doc;
-
+            $this->_key      = $key;
+        }
         return $this;
     }
     
@@ -48,6 +60,17 @@ class JForg_Dodb_Record extends Solar_Base {
     {
         if ( $name === 'document' )
             return $this->_document;
+    }
+
+    /**
+     * TODO: short description.
+     * 
+     * @return mixed
+     * @author Bahtiar Gadimov <bahtiar@gadimov.de>
+     */
+    public function getKey()
+    {
+        return $this->_key;
     }
 
 }
