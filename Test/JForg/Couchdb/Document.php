7b02639d5d8b1f4d5583c636eae4e6f4237bb359
Bahtiar Gadimov <mail@kalkin.de>
2010-10-17 20:49:37 +0200

    Generated Test for JForg_Couchdb_Document

---
+++ Test/JForg/Couchdb/Document.php
@@ -0,0 +1,340 @@
+<?php
+/**
+ * 
+ * Concrete class test.
+ * 
+ */
+class Test_JForg_Couchdb_Document extends Solar_Test {
+    
+    /**
+     * 
+     * Default configuration values.
+     * 
+     * @var array
+     * 
+     */
+    protected $_Test_JForg_Couchdb_Document = array(
+    );
+    
+    /**
+     * 
+     * Test -- Constructor.
+     * 
+     */
+    public function test__construct()
+    {
+        $actual = Solar::factory('JForg_Couchdb_Document');
+        $expect = 'JForg_Couchdb_Document';
+        $this->assertInstance($actual, $expect);
+    }
+    
+    /**
+     * 
+     * Test -- Implementation of Countable interface
+     * 
+     */
+    public function testCount()
+    {
+        $this->todo('stub');
+    }
+    
+    /**
+     * 
+     * Test -- Return the current element
+     * 
+     */
+    public function testCurrent()
+    {
+        $this->todo('stub');
+    }
+    
+    /**
+     * 
+     * Test -- Return the key of the current element
+     * 
+     */
+    public function testKey()
+    {
+        $this->todo('stub');
+    }
+    
+    /**
+     * 
+     * Test -- Move forward to next element
+     * 
+     */
+    public function testNext()
+    {
+        $this->todo('stub');
+    }
+    
+    /**
+     * 
+     * Test -- Whether a offset exists
+     * 
+     */
+    public function testOffsetExists()
+    {
+        $this->todo('stub');
+    }
+    
+    /**
+     * 
+     * Test -- Returns the value at specified offset.
+     * 
+     */
+    public function testOffsetGet()
+    {
+        $this->todo('stub');
+    }
+    
+    /**
+     * 
+     * Test -- Assigns a value to the specified offset.
+     * 
+     */
+    public function testOffsetSet()
+    {
+        $this->todo('stub');
+    }
+    
+    /**
+     * 
+     * Test -- Unsets an offset.
+     * 
+     */
+    public function testOffsetUnset()
+    {
+        $this->todo('stub');
+    }
+    
+    /**
+     * 
+     * Test -- Rewind the Iterator to the first element
+     * 
+     */
+    public function testRewind()
+    {
+        $this->todo('stub');
+    }
+    
+    /**
+     * 
+     * Test -- 
+     * 
+     */
+    public function testToArray()
+    {
+        $this->todo('stub');
+    }
+    
+    /**
+     * 
+     * Test -- Checks if current position is valid
+     * 
+     */
+    public function testValid()
+    {
+        $this->todo('stub');
+    }
+    
+    /**
+     * 
+     * Test -- Magic call implements "get...()" and "set...()" for params listed in the method name.
+     * 
+     */
+    public function test__call()
+    {
+        $this->todo('stub');
+    }
+    
+    /**
+     * 
+     * Test -- Get a property value
+     * 
+     */
+    public function test__get()
+    {
+        $this->todo('stub');
+    }
+    
+    /**
+     * 
+     * Test -- Checks if a dpcument property is set.
+     * 
+     */
+    public function test__isset()
+    {
+        $this->todo('stub');
+    }
+    
+    /**
+     * 
+     * Test -- Set a property value
+     * 
+     */
+    public function test__set()
+    {
+        $this->todo('stub');
+    }
+    
+    /**
+     * 
+     * Test -- Returns a string representation of the object.
+     * 
+     */
+    public function test__toString()
+    {
+        $this->todo('stub');
+    }
+    
+    /**
+     * 
+     * Test -- Checks if a property is unset
+     * 
+     */
+    public function test__unset()
+    {
+        $this->todo('stub');
+    }
+    
+    /**
+     * 
+     * Test -- Deletes the document from the database
+     * 
+     */
+    public function testDelete()
+    {
+        $this->todo('stub');
+    }
+    
+    /**
+     * 
+     * Test -- Returns the document id.
+     * 
+     */
+    public function testFetchDocumentId()
+    {
+        $this->todo('stub');
+    }
+    
+    /**
+     * 
+     * Test -- Returns the document sheme.
+     * 
+     */
+    public function testFetchDocumentSheme()
+    {
+        $this->todo('stub');
+    }
+    
+    /**
+     * 
+     * Test -- Returns all properties with values of a document.
+     * 
+     */
+    public function testFetchProperties()
+    {
+        $this->todo('stub');
+    }
+    
+    /**
+     * 
+     * Test -- Returns the name of the properties of a document
+     * 
+     */
+    public function testFetchPropetiesNames()
+    {
+        $this->todo('stub');
+    }
+    
+    /**
+     * 
+     * Test -- Returns the names of the special properties of a document
+     * 
+     */
+    public function testFetchSpecialPropertiesNames()
+    {
+        $this->todo('stub');
+    }
+    
+    /**
+     * 
+     * Test -- Returns a value of a special property
+     * 
+     */
+    public function testFetchSpecialProperty()
+    {
+        $this->todo('stub');
+    }
+    
+    /**
+     * 
+     * Test -- Are there some unsaved changes?
+     * 
+     */
+    public function testIsDirty()
+    {
+        $this->todo('stub');
+    }
+    
+    /**
+     * 
+     * Test -- Checks if the document is final
+     * 
+     */
+    public function testIsFinal()
+    {
+        $this->todo('stub');
+    }
+    
+    /**
+     * 
+     * Test -- Is the document already populated?
+     * 
+     */
+    public function testIsPopulated()
+    {
+        $this->todo('stub');
+    }
+    
+    /**
+     * 
+     * Test -- Populates the document with data.
+     * 
+     */
+    public function testPopulate()
+    {
+        $this->todo('stub');
+    }
+    
+    /**
+     * 
+     * Test -- Reloads the Document from the database and updates it's values
+     * 
+     */
+    public function testReload()
+    {
+        $this->todo('stub');
+    }
+    
+    /**
+     * 
+     * Test -- Saves the document to the dodb
+     * 
+     */
+    public function testSave()
+    {
+        $this->todo('stub');
+    }
+    
+    /**
+     * 
+     * Test -- Sets a special property to the document.
+     * 
+     */
+    public function testSetSpecialProperty()
+    {
+        $this->todo('stub');
+    }
+}
