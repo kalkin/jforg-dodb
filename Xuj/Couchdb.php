61e0da621f1a37d2b8f2eab26732dc8302cf7b71
Bahtiar Gadimov <mail@kalkin.de>
2010-10-17 14:58:14 +0200

    added couchdb

---
+++ Xuj/Couchdb.php
@@ -0,0 +1,18 @@
+<?php
+class Xuj_Couchdb extends Solar_Factory
+{
+
+    /**
+     * Default configuration values.
+     *
+     * @config string adapter The adapter class to use
+     * 
+     * @var mixed  Defaults to array(            ). 
+     * @since 2010-01-28
+     */
+    protected $_Xuj_Couchdb = array(
+        'adapter' => null,
+            );
+
+}
+
