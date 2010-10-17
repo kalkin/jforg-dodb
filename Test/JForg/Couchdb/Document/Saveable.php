0d2af3197b09a7af5e040b5a737b5c34b4f68421
Bahtiar Gadimov <mail@kalkin.de>
2010-10-17 20:08:25 +0200

    Added test class for JForg_Couchdb_Document_Saveable

---
+++ Test/JForg/Couchdb/Document/Saveable.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * 
+ * Concrete class test.
+ * 
+ */
+class Test_JForg_Couchdb_Document_Saveable extends Solar_Test {
+    
+    /**
+     * 
+     * Default configuration values.
+     * 
+     * @var array
+     * 
+     */
+    protected $_Test_JForg_Couchdb_Document_Saveable = array(
+    );
+    
+    /**
+     * 
+     * Test -- Constructor.
+     * 
+     */
+    public function test__construct()
+    {
+        $this->skip("is just an interface");
+    }
+}
