7215a6514a2ddb450ff81ebeec20323d5ab4e75a
Bahtiar Gadimov <mail@kalkin.de>
2010-10-17 20:07:40 +0200

    Adde doc to JForg_Couchdb_Document_Saveable

--- JForg/Couchdb/Document/Saveable.php
+++ JForg/Couchdb/Document/Saveable.php
@@ -1,5 +1,12 @@
 <?php
 interface JForg_Couchdb_Document_Saveable 
 {
+    /**
+     * Returns an array with document data. The structure is prepared for
+     * inserting a document in to couchdb
+     * 
+     * @return array
+     * @author Bahtiar Gadimov <bahtiar@gadimov.de>
+     */
     public function toArray();
 }
