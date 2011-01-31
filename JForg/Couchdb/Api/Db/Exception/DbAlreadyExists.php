b0a1596b509c1f13300cc06e3e1d9a88e99c2e6d
Bahtiar `kalkin-` Gadimov <mail@kalkin.de>
2011-02-01 00:44:23 +0100

    Deleted old exceptions

--- JForg/Couchdb/Api/Db/Exception/DbAlreadyExists.php
+++
@@ -1,3 +0,0 @@
-<?php
-class  JForg_Couchdb_Api_Db_Exception_DbAlreadyExists extends Solar_Exception{}
-
--- JForg/Couchdb/Api/Db/Exception/DbIllegalName.php
+++
@@ -1,2 +0,0 @@
-<?php
-class  JForg_Couchdb_Api_Db_Exception_DbIllegalName extends Solar_Exception{}
